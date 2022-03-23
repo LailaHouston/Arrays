44521980f4683903b0a29cfb41a4d3ea87693ab2
Laila Houston <laila.houston@students.jmcss.org>
2022-03-23 09:18:48 -0500

    arrays assignment

---
+++ array.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+
+<?php
+
+    //string
+    $name = 'coding is fun';
+
+
+    //integer
+    $name = 20;
+
+    //float
+    $name = 20.5784;
+
+    //BooLean
+    true = 1
+    false = 0
+
+    $names = array("Daniel","Dennis","Michael");
+    echo $names['1'];
+
+
+?>
+
+
+
+</body>
+</html>
